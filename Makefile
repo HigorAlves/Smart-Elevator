82bef54a56ebce91b301a6a71b969ab17cefd358
higoralves <goodyearalves@gmail.com>
2016-08-27 17:26:58 -0300

    Make file

---
+++ Makefile
@@ -0,0 +1,5 @@
+compila:
+	gcc main.c funcoes.c funcoes.h -o tp1
+
+clean:
+	rm -rf tp1
--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+/*lib do GETOPT */
+#include <unistd.h>
 
-int main(){
+/* Mostra a ajuda */
+void show_help(char *Passageiros) {
+  fprintf(stderr, "\
+    [uso] ./tp1 <opcoes>\n\
+    -sh         mostra essa tela e sai.\n\
+    -a ANDARES    seta a quantidade de andares.\n\
+    -c CAPACIDADE   seta a capacidade do elevador.\n\
+    -p NOME_ARQUIVO nome para o arquivo dos passageiros\n");
+  exit(-1) ;
+}
 
-return 0;
+int main(int argc, char **argv) {
+
+  int *q_andares = NULL, *capacidade = NULL;
+  /* Variavel para o nome do arquivo dos passageiros */
+  char *Passageiros = NULL;
+
+  if (argc < 2) show_help(argv[0]);
+
+  return 0 ;
 }
