5f9c3660d585fc2f72099338db6df45d7c95b44d
higoralves <goodyearalves@gmail.com>
2016-08-27 18:28:23 -0300

    Getopt funcionando 100%

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
 compila:
 	gcc main.c funcoes.c funcoes.h -o tp1
-	clear
-	@echo "Compilacao feita com sucesso, nome do arquivo tp1"
 clean:
 	rm -rf tp1
-	clear
 	@echo "Arquivos limpados com sucesso!"
--- main.c
+++ main.c
@@ -6,24 +6,25 @@
 /* Mostra a ajuda */
 void show_help(char *q_andares) {
   fprintf(stderr, "\
-    [uso] ./tp1 <opcoes>\n\
+    [uso] ./tp1 <opcoe> -o <metodo>\n\
     -h         mostra essa tela e sai.\n\
     -a ANDARES    seta a quantidade de andares.\n\
+    -p ARQ_PASSAGEIROS nome do arquivo dos passageiros\n\
     -c CAPACIDADE   seta a capacidade do elevador.\n\
-    -p NOME_ARQUIVO nome para o arquivo dos passageiros\n");
+    -o METODO use FIFO ou SJF como estrategia\n");
   exit(-1) ;
 }
 
 int main(int argc, char **argv) {
 
   int opcao;
-  char *q_andares = NULL, *capacidade = NULL, *Passageiros = NULL;
+  char *q_andares = NULL, *capacidade = NULL, *Passageiros = NULL, *metodo = NULL;
   /* Variavel para o nome do arquivo dos passageiros */
 
   /* Chama a tela de ajuda */
   if (argc < 2) show_help(argv[0]);
 
-  while((opcao = getopt(argc,argv,"hn:a:c:p")) > 0){
+  while((opcao = getopt(argc,argv,"hn:a:c:p:o:")) > 0){
     switch (opcao){
       case 'h':
         show_help(argv[0]);
@@ -35,12 +36,14 @@ int main(int argc, char **argv) {
         capacidade = optarg;
       break;
       case 'p':
-      Passageiros = optarg;
+        Passageiros = optarg;
+      break;
+      case 'o':
+        metodo = optarg;
       break;
     }
   }
 
-  printf("Andares: %s \n", q_andares);
 
   return 0 ;
 }
